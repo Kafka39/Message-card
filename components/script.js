66c314b7e59e460ab46365fc3939050f19b27e96
Kafka39 <kafka.0039@gmail.com>
2024-08-11 20:20:18 +0500

    v0.3 добавлена возможность отправлять сообщения. Сделана проверка на пустые строки на ссылки

--- components/script.js
+++ components/script.js
@@ -1,2 +1,41 @@
 const input = document.querySelector('.card-input')
 const content = document.querySelector('.card_content-wrapper')
+
+function isURL(value) {
+	try {
+		new URL(value)
+		return true
+	} catch (_) {
+		return false
+	}
+}
+
+input.addEventListener('keydown', event => {
+	const value = input.value
+	if (event.key === 'Enter') {
+		if (value.length === 0) return
+		event.preventDefault()
+		const words = value.split(' ')
+
+		htmlElement = words
+			.map(word => {
+				if (isURL(word)) {
+					return `<a class="message_content-link" href="${word}"
+				>${word}</a>`
+				}
+				return word
+			})
+			.join(' ')
+
+		content.innerHTML += `
+		
+		<div class="card-message_content">
+			<span class="message_content-span"
+			>
+			${htmlElement}</span>
+		</div>
+		
+		`
+		input.value = ''
+	}
+})
