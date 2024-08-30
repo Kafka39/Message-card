93e5b25ec650b031c0ed98f54b89b8c1ead0292e
Kafka39 <kafka.0039@gmail.com>
2024-08-31 01:10:50 +0500

    RESST, express, fetch v0.8

---
+++ components/app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const path = require('path')
+const pg = require('pg')
+
+const PORT = process.env.PORT || 3000
+const app = express()
+app.listen(PORT, () => `console.log(server started on port ${PORT})`)
+/////////////////////////////////////////
+
+CONTACT = [
+	{
+		id: 1,
+		first_name: 'Denis',
+		last_name: 'Oshi',
+		email: 'oshinodenis@gmail.com',
+	},
+]
+
+app.get('/api/contacts', (req, res) => {
+	res.status(200).json(CONTACT)
+})
+
+/////////////////////////////////////////
+async function database() {
+	const { Client } = pg
+	const client = new Client({
+		user: 'postgres',
+		password: 'kafka',
+		host: 'localhost',
+		port: 5432,
+		database: 'postgres',
+	})
+
+	await client.connect()
+
+	const res = await client.query(`SELECT * FROM users`)
+	console.log(res.rows)
+
+	await client.end()
+}
+database()
+
+////////////////////////////////////////////////////////////////
+app.use(express.static(path.resolve(__dirname, 'client')))
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+})
---
+++ components/client/script.js
@@ -0,0 +1,101 @@
+const input = document.querySelector('.card-input')
+const content = document.querySelector('.card_content-wrapper')
+const messageContainer = document.querySelector('.card-message_content')
+
+async function request(url, method = 'GET', data = null) {
+	try {
+		const headers = {}
+
+		let body
+
+		if (data) {
+			headers['Content-Type'] = 'application/json'
+			body = JSON.stringify(data)
+		}
+
+		const response = await fetch(url, {
+			method,
+			headers,
+			body,
+		})
+		return await response.json()
+	} catch (e) {
+		console.warn('Error: ', e.message)
+	}
+}
+
+async function getContact() {
+	const data = await request('/api/contacts')
+	console.log(data)
+
+	content.innerHTML += `
+		<div class="card-message_content">
+				<span class="message_content-span">${data[0].last_name} no ${data[0].first_name}</span>
+		</div>
+		`
+}
+getContact()
+
+function isURL(value) {
+	try {
+		new URL(value)
+		return true
+	} catch (_) {
+		return false
+	}
+}
+
+input.addEventListener('keydown', event => {
+	const value = input.value.trim()
+	const words = value.split(' ')
+
+	const htmlElement = words
+		.map(word => {
+			if (isURL(word)) {
+				return `<a class="message_content-link" target='_blank' href="${word} "
+				>${word}</a>`
+			}
+			return word
+		})
+		.join(' ')
+
+	if (event.key === 'Enter') {
+		event.preventDefault()
+		if (value.length === 0) return
+		content.innerHTML += `
+
+		<div class="card-message_content">
+			<span class="message_content-span">
+			${htmlElement}</span>
+		</div>
+		`
+		input.value = ''
+		content.scrollTop = content.scrollHeight
+		// content.scrollHeight = content.scrollTop
+	}
+})
+
+const btn = document.querySelector('.test_commit')
+const form = document.querySelector('.form')
+
+btn.addEventListener('click', event => {
+	event.preventDefault()
+
+	const formData = new FormData(form)
+
+	let valueObj = {}
+	for (let [name, value] of formData) {
+		valueObj[name] = value.trim()
+	}
+
+	if (valueObj.input1 !== '' && valueObj.input2 !== '') {
+		content.innerHTML += `
+		<div class="card-message_content">
+				<span class="message_content-span">
+				<span class="_object">First name:</span> ${valueObj.input1} 
+				<br/>
+				<span class="_object">Last name:</span> ${valueObj.input2}</span>
+		</div>
+		`
+	}
+})
